25d1adad195675072fa90ce577425d0d9f1d2f4d
varshithaputta <116485507+varshithaputta@users.noreply.github.com>
2023-11-19 20:49:21 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,72 +1,93 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('registrationForm');
+    registrationForm.addEventListener('submit', handleSubmit);
+  
+    // Load existing entries from localStorage
+    loadUserEntries();
+  });
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const terms = document.getElementById('terms').checked;
+  
+    // Validate date of birth within age range (18 to 55)
+    if (!isDateOfBirthValid(dob)) {
+      alert('Please enter a valid date of birth (age must be between 18 and 55).');
+      return;
     }
-
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
+  
+    // Validate email
+    if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
     }
-
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
-
-};
+  
+    // Add the entry to the table and localStorage
+    addUserEntry(name, email, password, dob, terms);
+  
+    // Clear the form
+    registrationForm.reset();
+  }
+  
+  function isDateOfBirthValid(dob) {
+    const currentDate = new Date();
+    const inputDate = new Date(dob);
+    const age = currentDate.getFullYear() - inputDate.getFullYear();
+  
+    return age >= 18 && age <= 55;
+  }
+  
+  function isValidEmail(email) {
+    // Basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  function addUserEntry(name, email, password, dob, terms) {
+    const userTableBody = document.getElementById('userTableBody');
+    const newRow = userTableBody.insertRow();
+  
+    const cellName = newRow.insertCell(0);
+    const cellEmail = newRow.insertCell(1);
+    const cellPassword = newRow.insertCell(2);
+    const cellDob = newRow.insertCell(3);
+    const cellTerms = newRow.insertCell(4);
+  
+    cellName.textContent = name;
+    cellEmail.textContent = email;
+    cellPassword.textContent = password;
+    cellDob.textContent = dob;
+    cellTerms.textContent = terms ? 'Yes' : 'No';
+  
+    // Save entry to localStorage
+    saveUserEntry(name, email, password, dob, terms);
+  }
+  
+  function loadUserEntries() {
+    const storedEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+  
+    storedEntries.forEach(entry => {
+      addUserEntry(entry.name, entry.email, entry.password, entry.dob, entry.terms);
+    });
+  }
+  
+  function saveUserEntry(name, email, password, dob, terms) {
+    const userEntries = JSON.parse(localStorage.getItem('userEntries')) || [];
+  
+    const newEntry = {
+      name: name,
+      email: email,
+      password: password,
+      dob: dob,
+      terms: terms
+    };
+  
+    userEntries.push(newEntry);
+    localStorage.setItem('userEntries', JSON.stringify(userEntries));
+  }
+  
